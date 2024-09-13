6f70c9d9870a9879eaa1bad274a1cbd96e5bd07c
Bruno Kaiser <bruno@bkaiser.ch>
2024-09-13 22:06:42 +0200

    fixing paths to logo images

--- libs/shared/ui/src/lib/modals/card-select.modal.ts
+++ libs/shared/ui/src/lib/modals/card-select.modal.ts
@@ -1,9 +1,10 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { TranslatePipe } from '@bk/pipes';
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonImg, IonItem, IonLabel, IonRow, ModalController } from '@ionic/angular/standalone';
 import { BkHeaderComponent } from '../structural/bk-header';
 import { Category } from '@bk/categories';
+import { ConfigService } from '@bk/util';
 
 @Component({
   selector: 'bk-card-select-modal',
@@ -31,7 +32,7 @@ import { Category } from '@bk/categories';
                     <ion-card-subtitle>{{ cat.name }}</ion-card-subtitle>
                   </ion-card-header>
                   <ion-card-content>
-                    <ion-img src="{{ 'assets/' + slug() + '/' + cat.name + '.png'}}" alt="{{ cat.name }}" />
+                    <ion-img src="{{ path() + cat.name + '.svg'}}" alt="{{ cat.name }}" />
                   </ion-card-content>
                 </ion-card>
               </ion-col>
@@ -44,9 +45,13 @@ import { Category } from '@bk/categories';
 })
 export class BkCardSelectModalComponent {
   private modalController = inject(ModalController);
+  protected configService = inject(ConfigService);
+
+  public baseImgixUrl = this.configService.getConfigString('cms_imgix_base_url');
 
   public categories = input.required<Category[]>();
   public slug = input.required<string>();
+  protected path = computed(() => this.baseImgixUrl + '/logo/' + this.slug() + '/');
   
   public async select(index: number): Promise<boolean> {
     return await this.modalController.dismiss(index, 'confirm');
