9d96c940cf72b59912891e54ab2667df8a6fcc2d
Bruno Kaiser <bruno@bkaiser.ch>
2024-10-14 13:24:59 +0200

    generating nx.json from ci.yml

---
+++ generate-nx.ts
@@ -0,0 +1,102 @@
+import * as fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();    // load environment variables from .env file
+const writeFile = fs.writeFile;
+const nxJsonPath = './nx.json';
+
+export const writeNxJson = () => {
+    const nxJson = `
+{
+  "$schema": "./node_modules/nx/schemas/nx-schema.json",
+  "namedInputs": {
+    "default": ["{projectRoot}/**/*", "sharedGlobals"],
+    "production": [
+      "default",
+      "!{projectRoot}/.eslintrc.json",
+      "!{projectRoot}/eslint.config.js",
+      "!{projectRoot}/**/?(*.)+(spec|test).[jt]s?(x)?(.snap)",
+      "!{projectRoot}/tsconfig.spec.json",
+      "!{projectRoot}/jest.config.[jt]s",
+      "!{projectRoot}/src/test-setup.[jt]s",
+      "!{projectRoot}/test-setup.[jt]s",
+      "!{projectRoot}/cypress/**/*",
+      "!{projectRoot}/**/*.cy.[jt]s?(x)",
+      "!{projectRoot}/cypress.config.[jt]s"
+    ],
+    "sharedGlobals": ["{workspaceRoot}/.github/workflows/ci.yml"]
+  },
+  "targetDefaults": {
+    "@angular-devkit/build-angular:application": {
+      "cache": true,
+      "dependsOn": ["^build"],
+      "inputs": ["production", "^production"]
+    },
+    "@nx/eslint:lint": {
+      "cache": true,
+      "inputs": [
+        "default",
+        "{workspaceRoot}/.eslintrc.json",
+        "{workspaceRoot}/.eslintignore",
+        "{workspaceRoot}/eslint.config.js"
+      ]
+    },
+    "@nx/jest:jest": {
+      "cache": true,
+      "inputs": ["default", "^production", "{workspaceRoot}/jest.preset.js"],
+      "options": {
+        "passWithNoTests": true
+      },
+      "configurations": {
+        "ci": {
+          "ci": true,
+          "codeCoverage": true
+        }
+      }
+    },
+    "@nx/js:tsc": {
+      "cache": true,
+      "dependsOn": ["^build"],
+      "inputs": ["production", "^production"]
+    }
+  },
+  "plugins": [
+    {
+      "plugin": "@nx/cypress/plugin",
+      "options": {
+        "targetName": "e2e",
+        "openTargetName": "open-cypress",
+        "componentTestingTargetName": "component-test",
+        "ciTargetName": "e2e-ci"
+      }
+    },
+    {
+      "plugin": "@nx/eslint/plugin",
+      "options": {
+        "targetName": "lint"
+      }
+    }
+  ],
+  "generators": {
+    "@nx/angular:application": {
+      "e2eTestRunner": "cypress",
+      "linter": "eslint",
+      "style": "scss",
+      "unitTestRunner": "jest"
+    }
+  },
+  "nxCloudAccessToken": "${process.env['NEXT_PUBLIC_NX_CLOUD_ACCESS_TOKEN']}"
+}
+  `;
+  writeFile(nxJsonPath, nxJson, (err) => {
+    if (err) {
+      console.error('nx.json file could not be generated with writeFile().');
+      console.error(err);
+      throw err;
+    } else {
+      console.log(`nx.json file generated correctly at ${nxJsonPath} \n`);
+    }
+  });
+};
+
+writeNxJson();
--- set-env.ts
+++ set-env.ts
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();    // load environment variables from .env file
 const writeFile = fs.writeFile;
-const targetPath = `./apps/${process.env['NEXT_PUBLIC_AUTH_TENANTID']}/src/environments/environment.ts`;
+const envPath = `./apps/${process.env['NEXT_PUBLIC_AUTH_TENANTID']}/src/environments/environment.ts`;
+const envProdPath = `./apps/${process.env['NEXT_PUBLIC_AUTH_TENANTID']}/src/environments/environment.prod.ts`;
 
 export const setEnv = () => {
 // example to read version from package.json: import { version as appVersion } from '../../package.json';
@@ -103,110 +104,123 @@ export const setEnv = () => {
     }
   };
 `;
-  writeFile(targetPath, envConfigFile, (err) => {
+  writeFile(envPath, envConfigFile, (err) => {
     if (err) {
       console.error('Angular environment.ts file could not be generated with writeFile().');
       console.error(err);
       throw err;
     } else {
-      console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+      console.log(`Angular environment.ts file generated correctly at ${envPath} \n`);
     }
   });
 };
 
-export const writeNxJson = () => {
-    const nxJson = `
-{
-  "$schema": "./node_modules/nx/schemas/nx-schema.json",
-  "namedInputs": {
-    "default": ["{projectRoot}/**/*", "sharedGlobals"],
-    "production": [
-      "default",
-      "!{projectRoot}/.eslintrc.json",
-      "!{projectRoot}/eslint.config.js",
-      "!{projectRoot}/**/?(*.)+(spec|test).[jt]s?(x)?(.snap)",
-      "!{projectRoot}/tsconfig.spec.json",
-      "!{projectRoot}/jest.config.[jt]s",
-      "!{projectRoot}/src/test-setup.[jt]s",
-      "!{projectRoot}/test-setup.[jt]s",
-      "!{projectRoot}/cypress/**/*",
-      "!{projectRoot}/**/*.cy.[jt]s?(x)",
-      "!{projectRoot}/cypress.config.[jt]s"
-    ],
-    "sharedGlobals": ["{workspaceRoot}/.github/workflows/ci.yml"]
-  },
-  "targetDefaults": {
-    "@angular-devkit/build-angular:application": {
-      "cache": true,
-      "dependsOn": ["^build"],
-      "inputs": ["production", "^production"]
-    },
-    "@nx/eslint:lint": {
-      "cache": true,
-      "inputs": [
-        "default",
-        "{workspaceRoot}/.eslintrc.json",
-        "{workspaceRoot}/.eslintignore",
-        "{workspaceRoot}/eslint.config.js"
-      ]
-    },
-    "@nx/jest:jest": {
-      "cache": true,
-      "inputs": ["default", "^production", "{workspaceRoot}/jest.preset.js"],
-      "options": {
-        "passWithNoTests": true
+// /apps/TENANT/src/environments/environment.prod.ts
+export const setProdEnv = () => {
+    const prodEnvConfigFile = `
+    import {BkEnvironment} from '@bk/util';
+  
+    export const environment: BkEnvironment = {
+      production: true,
+      useEmulators: false,
+      firebase: {
+        apiKey: '${process.env['NEXT_PUBLIC_FIREBASE_API_KEY']}',
+        authDomain: '${process.env['NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN']}',
+        databaseUrl: '${process.env['NEXT_PUBLIC_FIREBASE_DATABASE_URL']}',
+        projectId: '${process.env['NEXT_PUBLIC_FIREBASE_PROJECT_ID']}',
+        storageBucket: '${process.env['NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET']}',
+        messagingSenderId: '${process.env['NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID']}',
+        appId: '${process.env['NEXT_PUBLIC_FIREBASE_APP_ID']}',
+        measurementId: '${process.env['NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID']}',
+        appcheckRecaptchaEnterpriseKey: '${process.env['NEXT_PUBLIC_FIREBASE_RECAPTCHA_KEY']}'
       },
-      "configurations": {
-        "ci": {
-          "ci": true,
-          "codeCoverage": true
-        }
+      app: {
+        domain: '${process.env['NEXT_PUBLIC_APP_DOMAIN']}',
+        imgixBaseUrl: '${process.env['NEXT_PUBLIC_APP_IMGIX_BASE_URL']}',
+        rootUrl: '/public/welcome',
+        logoUrl: 'tenant/${process.env['NEXT_PUBLIC_AUTH_TENANTID']}/logo/logo_round.svg',
+        welcomeBannerUrl: 'tenant/${process.env['NEXT_PUBLIC_AUTH_TENANTID']}/app/welcome.jpg',
+        notfoundBannerUrl: 'tenant/${process.env['NEXT_PUBLIC_AUTH_TENANTID']}/app/not-found.jpg',
+        osiLogoUrl: 'logo/general/osi.svg',
+        showDateOfBirth: 'admin',
+        showDateOfDeath: 'admin',
+        showGender: 'admin',
+        showTaxId: 'admin',
+        showBexioId: 'admin',
+        showTags: 'admin',
+        showNotes: 'admin',
+        showMemberships: 'admin',
+        showOwnerships: 'admin',
+        showComments: 'admin',
+        showDocuments: 'admin'
+      },
+      auth: {
+        tenantId: '${process.env['NEXT_PUBLIC_AUTH_TENANTID']}',
+        loginUrl: '/auth/login',
+        passwordResetUrl: '/auth/pwdreset',
+      },
+      thumbnail: {
+        width: 200,
+        height: 300
+      },
+      dpo: {
+        email: '${process.env['NEXT_PUBLIC_DPO_EMAIL']}',
+        name: '${process.env['NEXT_PUBLIC_DPO_NAME']}'
+      },
+      git: {
+        repo: '${process.env['NEXT_PUBLIC_GIT_REPO']}',
+        org: '${process.env['NEXT_PUBLIC_GIT_ORG']}',
+        issueUrl: 'https://github.com/${process.env['NEXT_PUBLIC_GIT_ORG']}/${process.env['NEXT_PUBLIC_GIT_REPO']}/issues/new'
+      },
+      services: {
+        gmapKey: '${process.env['NEXT_PUBLIC_SVC_GMAP_KEY']}'
+      },
+      i18n: {
+        availableLangs: '["en","de","fr","es","it"]',
+        defaultLanguage: 'de',
+        fallbackLanguage: 'de',
+        folderUrl: 'assets/i18n/',
+        locale: 'de-ch',
+        logMissingKeys: true,
+        userLanguage: 'de',
+        useFallbackTranslation: true
+      },
+      operator: {
+        name: '${process.env['NEXT_PUBLIC_OP_NAME']}',
+        street: '${process.env['NEXT_PUBLIC_OP_STREET']}',
+        zipcode: '${process.env['NEXT_PUBLIC_OP_ZIP']}',
+        city: '${process.env['NEXT_PUBLIC_OP_CITY']}',
+        email: '${process.env['NEXT_PUBLIC_OP_EMAIL']}',
+        web: '${process.env['NEXT_PUBLIC_OP_WEB']}'
+      },
+      settingsDefaults: {
+        avatarUsage: 3,
+        gravatarEmail: '',
+        invoiceDelivery: 1,
+        maxYear: 2050,
+        minYear: 1850,
+        nameDisplay: 0,
+        newsDelivery: 2,
+        personSortCriteria: 1,
+        showArchivedData: false,
+        showDebugInfo: false,
+        showTestData: false,
+        toastLength: 3000,
+        useFaceId: false,
+        useTouchId: false
       }
-    },
-    "@nx/js:tsc": {
-      "cache": true,
-      "dependsOn": ["^build"],
-      "inputs": ["production", "^production"]
-    }
-  },
-  "plugins": [
-    {
-      "plugin": "@nx/cypress/plugin",
-      "options": {
-        "targetName": "e2e",
-        "openTargetName": "open-cypress",
-        "componentTestingTargetName": "component-test",
-        "ciTargetName": "e2e-ci"
-      }
-    },
-    {
-      "plugin": "@nx/eslint/plugin",
-      "options": {
-        "targetName": "lint"
-      }
-    }
-  ],
-  "generators": {
-    "@nx/angular:application": {
-      "e2eTestRunner": "cypress",
-      "linter": "eslint",
-      "style": "scss",
-      "unitTestRunner": "jest"
-    }
-  },
-  "nxCloudAccessToken": "${process.env['NEXT_PUBLIC_NX_CLOUD_ACCESS_TOKEN']}"
-}
+    };
   `;
-  writeFile('./nx.json', nxJson, (err) => {
-    if (err) {
-      console.error('nx.json file could not be generated with writeFile().');
-      console.error(err);
-      throw err;
-    } else {
-      console.log(`nx.json file generated correctly at ./nx.json \n`);
-    }
-  });
-};
+    writeFile(envProdPath, prodEnvConfigFile, (err) => {
+      if (err) {
+        console.error('Angular environment.prod.ts file could not be generated with writeFile().');
+        console.error(err);
+        throw err;
+      } else {
+        console.log(`Angular environment.ts file generated correctly at ${envProdPath} \n`);
+      }
+    });
+  };
 
 setEnv();
-writeNxJson();
+setProdEnv();
